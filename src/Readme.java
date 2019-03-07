949309c0b9ee222cc4c63e10bb212861668c1951
Andreas Fischer <andreas.fischer@fielmann.com>
2019-03-07 13:58:26 +0100

    Readme

--- src/Readme.java
+++ src/Readme.java
@@ -9,7 +9,7 @@ public class Readme {}
  * 
  * 
  * 
- * 07.03.2019	002		Gui.class ergaenzt um CardLayout
+ * 07.03.2019	002		Gui.class ergaenzt um CardLayout.
  * 
  * 04.03.2019	001		Erste Version
  * 
