3c8062c5b0ccecd11adffa6e0c16286b2c6198a4
Andy Fischer <info@andyland.info>
2020-01-11 22:35:32 +0100

    Model und readme.java angepasst

--- src/Readme.java
+++ src/Readme.java
@@ -1,24 +1,25 @@
 
-public class Readme {}
-
+public class Readme {
+}
 
 /*
- * TestProject
- * zum Sammeln unterschiedlicher Ideen und Konzepte
+ * TestProject zum Sammeln unterschiedlicher Ideen und Konzepte
  * 
- * 21.03.2019	007		ControllerImpl.selectView(): Views werden immer aufgerufen.
+ * 11.01.2020 008 Erste Versuche mit VSCode als neuer IDE incl Git-Support etc.
  * 
- * 18.03.2019	006		View_Test1.java ist die Vorlage fuer weitere Views.
+ * 21.03.2019 007 ControllerImpl.selectView(): Views werden immer aufgerufen.
  * 
- * 14.03.2019	005		Neu: BTLabel, View_Password erweitert.
+ * 18.03.2019 006 View_Test1.java ist die Vorlage fuer weitere Views.
  * 
- * 14.03.2019	004		View mit Password, bei falschem PW bzw. Abbruch der Eingabe wird zur vorherigen View gesprungen.
+ * 14.03.2019 005 Neu: BTLabel, View_Password erweitert.
  * 
- * 13.03.2019	003		BottomFrame.class
+ * 14.03.2019 004 View mit Password, bei falschem PW bzw. Abbruch der Eingabe
+ * wird zur vorherigen View gesprungen.
  * 
- * 07.03.2019	002		Gui.class ergaenzt um CardLayout.
+ * 13.03.2019 003 BottomFrame.class
  * 
- * 04.03.2019	001		Erste Version
+ * 07.03.2019 002 Gui.class ergaenzt um CardLayout.
+ * 
+ * 04.03.2019 001 Erste Version
  * 
  */
-
--- src/model/Model.java
+++ src/model/Model.java
@@ -7,52 +7,49 @@ import java.util.Observer;
 import ch.bildspur.artnet.ArtNetClient;
 import ch.bildspur.artnet.ArtNetException;
 import ch.bildspur.artnet.ArtNetServer;
-import ch.bildspur.artnet.packets.ArtNetPacket;
 import ch.bildspur.artnet.packets.ArtPollReplyPacket;
-import ch.bildspur.artnet.packets.PacketType;
 
 public class Model extends Observable {
-	
+
 	private static String NAME = "Test-Project";
-	private static String VERSION = "005";
+	private static String VERSION = "008";
 
 	private ArtNetServer artNetServer;
 	private ArtNetClient artNetClient;
-	
+
 	public Model() {
 		this.init();
-		//this.initArtNetServer();
-		//this.initArtNetClient();
+		// this.initArtNetServer();
+		// this.initArtNetClient();
 	}
-	
+
 	private void init() {
-		
+
 	}
-	
+
 	public String getVersion() {
 		return VERSION;
 	}
-	
+
 	public String getName() {
 		return NAME;
 	}
-	
+
 	public void addObserver(Observer pObserver) {
 		super.addObserver(pObserver);
 	}
-	
+
 	public void makeNotify(int pObserverType, int pValue, String pText, boolean pBool) {
 		ObserverData obsData = new ObserverData(pObserverType, pValue, pText, pBool);
 		this.setChanged();
-		this.notifyObservers( obsData );
+		this.notifyObservers(obsData);
 	}
-	
-	
+
 	private void initArtNetServer() {
 		ArtPollReplyPacket aprp = new ArtPollReplyPacket();
 		aprp.setLongName("Deine Mudder");
 		aprp.setShortName("DM");
-		
+
 		this.artNetServer = new ArtNetServer();
 		this.artNetServer.setDefaultReplyPacket(aprp);
 		try {
@@ -61,20 +58,18 @@ public class Model extends Observable {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
-		
+
 	}
-	
+
 	private void initArtNetClient() {
-		this.artNetClient = new ArtNetClient( null );
+		this.artNetClient = new ArtNetClient(null);
 		byte[] dmxData = new byte[512];
 		this.artNetClient.broadcastDmx(1, 1, dmxData);
 	}
-	
-	
-//	public void sendDMX() {
-//		byte[] dmxData = new byte[512];
-//		this.artNetClient.broadcastDmx(01, 01, dmxData);
-//	}
-	
-}//model
+
+	// public void sendDMX() {
+	// byte[] dmxData = new byte[512];
+	// this.artNetClient.broadcastDmx(01, 01, dmxData);
+	// }
+
+}// model
